160a21b804d74d055eaf2454a406999dac369ece
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-02 15:36:05 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,12 @@
+var chocolateBars = [snickers, hundred grand, kitkat, skittles]
+
+//does not alter the original array
+function addElementToBeginningOfArray(array, newElement) {
+  return [newElement, ...array]
+}
+
+//does alter the original array
+function destructivelyAddElementToBeginningOfArray(array, newElement) {
+  return array.unshift(newElement)
+}
+
